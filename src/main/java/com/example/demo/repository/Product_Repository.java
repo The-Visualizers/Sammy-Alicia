cfa390435723fbc06300f383f9bab9c8e9a7dd9b
Sammy Ester <99742926+sester2@users.noreply.github.com>
2023-11-15 12:15:58 -0600

    Had to adjust my imports and methods

--- src/main/java/com/example/demo/repository/Product_Repository.java
+++ src/main/java/com/example/demo/repository/Product_Repository.java
@@ -10,12 +10,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.model.ProductTable;
 
-public interface Product_Repository extends JpaRepository<ProductTable, Long> {
-    @Query("SELECT p FROM ProductTable p WHERE (:id is null OR p.id = :id) AND p.name LIKE %:query%")
+public interface Product_Repository extends JpaRepository<ProductTable, Long>{
+
+     @Query("SELECT p FROM ProductTable p WHERE (:id is null OR p.id = :id) AND p.name LIKE %:query%")
     List<ProductTable> searchProducts(@Param("id") Optional<Long> id, @RequestParam("query") String query);
 
     List<ProductTable> findByName(String name);
 
     Optional<ProductTable> findById(Long id);
-    
 }
